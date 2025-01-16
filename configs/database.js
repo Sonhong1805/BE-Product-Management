db161215dfa21213286147f92329101cae6cb512
Sonhong1805 <nguyenhongson18052003@gmail.com>
2025-01-16 18:39:51 +0700

    deploy 4

--- configs/database.js
+++ configs/database.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connection successfully");
   } catch (error) {
     console.error("Database connection error:", error.message);
